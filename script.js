df764f9c13158ba7fe0826d391166c8050fab1ea
kazazura <kazazura@gmail.com>
2025-03-09 13:45:02 +0800

    Add function for resetting the grid and event listener for the button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const gridContainer = document.querySelector(".grid-container");
+const btnSquare = document.querySelector(".squares");
+
 function createGrid(size) {
     for (let i = 0; i < size; i++) {
         const column = document.createElement("div");
@@ -12,7 +14,27 @@ function createGrid(size) {
             column.appendChild(row);
         }
         gridContainer.appendChild(column);
+
     }
 }
-createGrid(50);
+
+createGrid(16);
+
+function resetGrid() {
+    const input = prompt("Enter the a number: ");
+    if (input > 100) {
+        alert("The number of squares must be equal to or less than 100.");
+        createGrid(16);
+    } else {
+        createGrid(input);
+    }
+}
+
+btnSquare.addEventListener('click', () => {
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    resetGrid();
+});
+
 
